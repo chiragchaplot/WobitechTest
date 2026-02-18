9c80dab530b61c80ba5e8b3009d5c55f033f5986
Chirag Chaplot <chirag.chaplot@bp.com>
2026-02-18 16:49:02 +1100

    Initial Commit

---
+++ Wobitech Test/Wobitech_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wobitech_TestApp.swift
+//  Wobitech Test
+//
+//  Created by Chirag Chaplot on 18/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct Wobitech_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Wobitech TestTests/Wobitech_TestTests.swift
@@ -0,0 +1,16 @@
+//
+//  Wobitech_TestTests.swift
+//  Wobitech TestTests
+//
+//  Created by Chirag Chaplot on 18/2/2026.
+//
+
+import Testing
+
+struct Wobitech_TestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
